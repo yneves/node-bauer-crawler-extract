8c73039e23a2bb2d35d768bef500978397d6768e
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-07-25 14:19:28 -0300

    update crawler, array extract

--- test/sample/sample.js
+++ test/sample/sample.js
@@ -2,31 +2,39 @@
 
 "use strict";
 
-var fs = require("fs");
 var assert = require("assert");
 var Crawler = require("bauer-crawler");
 
 var crawler = new Crawler();
 
-crawler.require(__dirname + "/../../");
+crawler.loadPlugin(__dirname + "/../../");
 
 crawler.ready(function() {
   
   this.promise()
-    .extract(__dirname + "/sample.json",{
-      json: true,
-      extract: "$..url"
-    })
+    .extract(__dirname + "/sample.json","$..url")
     .then(function(values) {
       assert.deepEqual(values,["/","/"]);
+    })
+    .extract(__dirname + "/sample.json",["$..url","$.ids[*].title"])
+    .then(function(values) {
+      assert.deepEqual(values,[
+        ["/","/"],
+        ["Go back to the home page","Go back to the home page"]
+      ]);
     })
     .extract(__dirname + "/sample.txt",/([0-9]{2}\/[0-9]{2}\/[0-9]{2,4})/g)
     .then(function(values) {
       assert.deepEqual(values,["13/09/2013","02/09/2013"]);
     })
+    .extract(__dirname + "/sample.txt",[/([0-9]{2}\/[0-9]{2}\/[0-9]{2,4})/g,/(Declara)/])
+    .then(function(values) {
+      assert.deepEqual(values,[
+        ["13/09/2013","02/09/2013"],
+        ["Declara"]
+      ]);
+    })
     .exit();
-  
-  
 });
 
 crawler.start();
